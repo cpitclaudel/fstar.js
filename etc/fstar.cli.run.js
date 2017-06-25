b6a613335da6b0366c9badb5a3b36eb23df4e8b8
Clément Pit--Claudel <clement.pitclaudel@live.com>
2017-06-25 01:01:15 -0400

    cli: Refactor fstar.cli's tests

--- etc/fstar.cli.run.js
+++ etc/fstar.cli.run.js
@@ -1,10 +1,13 @@
-#!/usr/bin/env nodejs
-/* global require */
-JSOO_FStar = require('../build/js/fstar.core.js'); /* exported JSOO_FStar */
-JSOO_FStar_Stdlib = require('../build/js/fstar.stdlib.js'); /* exported JSOO_FStar_Stdlib */
-FStar = require('../lib/fstar.driver.js');
-FStar.settings.catchExceptions = false;
-console.log(FStar.verifySync("test.fst", "module Test\n\nopen List\nlet a = List.length [1]",
-                             ["--lax",
-                              "--admit_smt_queries", "true",
-                              "--fstar_home", "/fstar"]));
+/* global require JSOO_FStar:true JSOO_FStar_Stdlib:true FStarDriver:true */
+if (typeof(require) !== 'undefined') {
+    JSOO_FStar = require('../build/js/fstar.core.js'); /* exported JSOO_FStar */
+    JSOO_FStar_Stdlib = require('../build/js/fstar.stdlib.js'); /* exported JSOO_FStar_Stdlib */
+    FStarDriver = require('../lib/fstar.driver.js');
+}
+
+var fname = "test.fst";
+var fcontents = "module Test"; // "module Test\n\nopen List\nlet a = List.length [1]";
+var args = ["--lax", "--admit_smt_queries", "true", "--fstar_home", "/fstar"];
+
+FStarDriver.settings.catchExceptions = false;
+console.log(FStarDriver.cli.verifySync(fname, fcontents, args));
