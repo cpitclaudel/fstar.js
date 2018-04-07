f4ca94d232cf7e26bf3443a1193b209ea03e24f4
Clément Pit-Claudel <clement.pitclaudel@live.com>
2018-04-07 09:51:55 -0400

    js2ts: Port fstar.worker.utils

--- lib/fstar.worker.utils.ts
+++ lib/fstar.worker.utils.ts
@@ -1,57 +1,58 @@
-"use strict";
-/* global self FStar: true */
+namespace FStar.WorkerUtils {
+    export let DEBUG: boolean = true;
 
-FStar.WorkerUtils = FStar.WorkerUtils || {};
+    export function debug(...msgs: any[]) {
+        if (DEBUG) {
+            console.debug(...msgs);
+        }
+    }
 
-(function () {
-    var WorkerUtils = FStar.WorkerUtils;
-
-    WorkerUtils.DEBUG = true;
-    WorkerUtils.debug = function() {
-        WorkerUtils.DEBUG && console.debug.apply(console, arguments);
-    };
-
-    WorkerUtils.postMessage = function(kind) {
-        return function(payload) {
-            self.postMessage({ kind: kind, payload: payload });
+    export function postMessage(kind: string) {
+        if (!(self instanceof DedicatedWorkerGlobalScope)) {
+            throw new Error("This method only works in dedicated workers.");
+        }
+        return (payload: any): void => {
+            (self as DedicatedWorkerGlobalScope).postMessage({ kind, payload });
         };
-    };
+    }
 
-    WorkerUtils.loadBinaries = function () {
+    export function loadBinaries() { // FIXME rename // FIXME merge all these files
         self.importScripts("./fstar.core.js",
                            "./fstar.driver.js",
                            "./z3smt2w.js",
                            "./fstar.smtdriver.js");
-    };
+    }
 
-    WorkerUtils.assert = function(condition, message) {
+    export function assert(condition: boolean, message: string | null) {
         if (!condition) {
             throw (message || "assertion failed");
         }
-    };
+    }
 
-    WorkerUtils.fetchSync = function fetchSync(url, responseType) {
-        var xhr = new XMLHttpRequest();
+    export function fetchSync(url: string, responseType: XMLHttpRequestResponseType) {
+        const xhr = new XMLHttpRequest();
         xhr.responseType = responseType;
         xhr.open("GET", url, false);
         xhr.send(null);
-        if (xhr.status != 200) {
-            throw "Failed to download " + url;
+        if (xhr.status !== 200) {
+            throw new Error(`Failed to download ${url}`);
         }
         return xhr.response;
-    };
+    }
 
-    WorkerUtils.fetchAsync = function fetchAsync(url, responseType, onProgress, onLoad) {
-        var xhr = new XMLHttpRequest();
+    export function fetchAsync(url: string, responseType: XMLHttpRequestResponseType,
+                               onProgress: (evt: ProgressEvent) => void,
+                               onLoad: (contents: any) => void) {
+        const xhr = new XMLHttpRequest();
         xhr.responseType = responseType;
         xhr.addEventListener("progress", onProgress);
-        xhr.addEventListener("load", function () {
-            if (xhr.status != 200) {
-                throw "Failed to download " + url;
+        xhr.addEventListener("load", () => {
+            if (xhr.status !== 200) {
+                throw new Error(`Failed to download ${url}`);
             }
             onLoad(xhr.response);
         });
         xhr.open("GET", url, true);
         xhr.send(null);
-    };
-}());
+    }
+}
