cb3e9456fa2061907d116bb333ceed4ef9329fbf
Clément Pit-Claudel <clement.pitclaudel@live.com>
2018-04-08 12:47:59 -0400

    Add license headers

--- lib/fstar.cli.client.ts
+++ lib/fstar.cli.client.ts
@@ -1,3 +1,21 @@
+/* Run F* on whole files and print the command line output (user side).
+
+Copyright (C) 2017-2018 Clément Pit-Claudel
+Author: Clément Pit-Claudel <clement.pitclaudel@live.com>
+URL: https://github.com/cpitclaudel/fstar.js
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
 namespace FStar.CLI {
     const _ = FStar._;
     import Utils = FStar.ClientUtils;
--- lib/fstar.driver.ts
+++ lib/fstar.driver.ts
@@ -1,3 +1,21 @@
+/* Initialize, connect to Z3, and run JSOO's F* module.
+
+Copyright (C) 2017-2018 Clément Pit-Claudel
+Author: Clément Pit-Claudel <clement.pitclaudel@live.com>
+URL: https://github.com/cpitclaudel/fstar.js
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
 interface JSOOFStarOptions {
     console?: typeof console;
     Object?: typeof Object;
